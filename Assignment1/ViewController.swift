04b2072d2e83903f716c4090aade5c447b0ba206
George Pantazis <gps@Georges-MacBook-Pro.local>
2016-04-09 18:45:02 +1000

    First Commit

---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by George Pantazis on 9/04/2016.
+//  Copyright © 2016 PAN Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
